031b10674ce8e7e358b7c0529282b4cd96ee15fd
BlazerR <br229@njit.edu>
2017-03-20 17:59:19 -0400

    1

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=br229';
+    $username = 'br229';
+    $password = '1Rm2Gizav';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
